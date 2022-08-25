6cdf8003768b35b03ae6beea799182f7c8e79b4a
faris37m <farisizwanfauzi@gmail.com>
2022-08-26 00:12:57 +0800

    push rini

--- function/Menu.js
+++ function/Menu.js
@@ -4,7 +4,7 @@ const knex = require("../connection.js");
 async function getMenu() {
     let result = null;
   
-    result = await knex.connect(`menu`).where(`menuStatusCode`, 1)
+    result = await knex.connect(`menu`).select("menuCode as menu_code ", "menuName as menu_name" , "menuDescription as menu_desc" , "menuPrice as menu_price" , "menuId as menu_id").where(`menuStatusCode`, 1)    
   
     console.log(result);
   
