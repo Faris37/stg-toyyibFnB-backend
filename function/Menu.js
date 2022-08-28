b3adf5dceb3d416eb8726f35d2aa1ca91d2fd7b7
Hishamudin98 <hishamudin.ali@gmail.com>
2022-08-28 13:36:28 +0800

    add get menu for pos

--- function/Menu.js
+++ function/Menu.js
@@ -1,17 +1,49 @@
 const knex = require("../connection.js");
 
-
 async function getMenu() {
-    let result = null;
-  
-    result = await knex.connect(`menu`).select("menuCode as menu_code ", "menuName as menu_name" , "menuDescription as menu_desc" , "menuPrice as menu_price" , "menuId as menu_id" , "menuImage as menu_image", "menu_category.fkCategory as fkcat_id")
-    .leftJoin('menu_category', 'menu.menuId', '=', 'menu_category.fkMenu').where(`menu.menuStatusCode`, 1)    
-  
-    console.log(result);
-  
-    return result;
-  }
+  let result = null;
+
+  result = await knex
+    .connect(`menu`)
+    .select(
+      "menuCode as menu_code ",
+      "menuName as menu_name",
+      "menuDescription as menu_desc",
+      "menuPrice as menu_price",
+      "menuId as menu_id",
+      "menuImage as menu_image",
+      "menu_category.fkCategory as fkcat_id"
+    )
+    .leftJoin("menu_category", "menu.menuId", "=", "menu_category.fkMenu")
+    .where(`menu.menuStatusCode`, 1);
+
+  console.log(result);
+
+  return result;
+}
+
+async function getMenuPOS() {
+  let result = null;
+
+  result = await knex
+    .connect(`menu`)
+    .select(
+      "menuCode as menu_code ",
+      "menuName as menu_name",
+      "menuDescription as menu_desc",
+      "menuPrice as menu_price",
+      "menuId as menu_id",
+      "menuImage as menu_image",
+      "menuCategory as menu_category"
+    )
+    .where(`menu.menuStatusCode`, 1);
+
+  console.log(result);
+
+  return result;
+}
 
 module.exports = {
-    getMenu,
-  };
+  getMenu,
+  getMenuPOS
+};
