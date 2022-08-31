2ddc23e3aac2916143f1a588baf078cd71babccb
Hishamudin98 <hishamudin.ali@gmail.com>
2022-09-01 03:25:43 +0800

    changes response

--- api/Payment/tblorderCallbackURL.js
+++ api/Payment/tblorderCallbackURL.js
@@ -31,7 +31,7 @@ router.post("/", async (req, res) => {
     logger.info( "refno: " + refno + " status: " + status + " reason: " + reason + " billcode: " + billcode + " order_id: " + order_id + " amount: " + amount + " transaction_time: " + transaction_time);
 
     console.log('req', req.body)
-    console.log("param2", param[0]);
+    console.log("refno", req.body.refno);
     result = {
       status: 200,
       message: "Success",
