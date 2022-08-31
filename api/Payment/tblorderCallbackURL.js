ccebf625ab99c69816240e62d58b531da017f8ff
Hishamudin98 <hishamudin.ali@gmail.com>
2022-09-01 00:19:55 +0800

    add log files

--- api/Payment/tblorderCallbackURL.js
+++ api/Payment/tblorderCallbackURL.js
@@ -3,50 +3,48 @@ const router = express.Router(); // MUST HAVE
 const { Console } = require("console");
 const fs = require("fs");
 
-
 router.post("/", async (req, res) => {
-    let param = null;
-    let result = null;
+  let param = null;
+  let result = null;
 
+  let refno = null;
+  let status = null;
+  let reason = null;
+  let billcode = null;
+  let order_id = null;
+  let amount = null;
+  let transaction_time = null;
 
-    let refno = null;
-    let status = null;
-    let reason = null;
-    let billcode = null;
-    let order_id = null;
-    let amount = null;
-    let transaction_time = null;
+  try {
+    // BIND PARAMETER TO VARIABLES
+    param = req.body;
 
+    refno = param.refno;
+    status = param.status;
+    reason = param.reason;
+    billcode = param.billcode;
+    order_id = param.order_id;
+    amount = param.amount;
+    transaction_time = param.transaction_time;
 
-    try {
+    fs.appendFile("callback.txt", param, function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    });
 
-        // BIND PARAMETER TO VARIABLES
-        param = req.body;
-
-        refno = param.refno;
-        status = param.status;
-        reason = param.reason;
-        billcode = param.billcode;
-        order_id = param.order_id;
-        amount = param.amount;
-        transaction_time = param.transaction_time;
-
-        fs.appendFile('callback.txt', param);
-        
-        result = {
-            response: 200,
-            status: "berjaya",
-            message: "Callback Berjaya.",
-            data:param,
-        };
-
-    } catch (error) {
-        console.log(error); // LOG ERROR
-        result = {
-            message: `API Error`,
-        };
-    }
-    res.status(200).json(result);
+    result = {
+      response: 200,
+      status: "berjaya",
+      message: "Callback Berjaya.",
+      data: param,
+    };
+  } catch (error) {
+    console.log(error); // LOG ERROR
+    result = {
+      message: `API Error`,
+    };
+  }
+  res.status(200).json(result);
 });
 
 module.exports = router;
