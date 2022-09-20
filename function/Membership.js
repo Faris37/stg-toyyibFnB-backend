584ced8f1f740fff416c6f4564fa82e15126e34a
Hishamudin98 <hishamudin.ali@gmail.com>
2022-09-20 14:00:06 +0800

    modify membership

--- function/Membership.js
+++ function/Membership.js
@@ -6,7 +6,7 @@ async function getMembership(mmberID) {
 
   result = await knex.connect(`membership`).where(`membershipNo`, mmberID);
 
-  console.log('result', result);
+  console.log("result", result);
 
   if (result.length > 0) {
     return result;
@@ -15,6 +15,25 @@ async function getMembership(mmberID) {
   }
 }
 
+async function getMembershipPOS(membership_no) {
+  let result = null;
+
+  result = await knex
+    .connect(`membership`)
+    .join('user', 'membership.fkUserId', '=', 'user.userId')
+    .select("userFullName as name", "membershipNo as membership_no", "membershipPoints as point", "membershipStatus as status" , "membershipExpiryDate as expiry_date", "membershipDateCreated as join_date")
+    .where(`membershipNo`, membership_no);
+
+  console.log("result", result);
+
+  if (result.length > 0) {
+    return result[0];
+  } else {
+    return false;
+  }
+}
+
 module.exports = {
   getMembership,
+  getMembershipPOS
 };
